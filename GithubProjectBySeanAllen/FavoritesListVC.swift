f130060abadc9d85e69baf8ff1f3158ae5067583
dhrushit-RIT <70442122+dhrushit-RIT@users.noreply.github.com>
2024-09-10 18:16:39 -0700

    first page

---
+++ GithubProjectBySeanAllen/FavoritesListVC.swift
@@ -0,0 +1,28 @@
+//
+//  FavoritesListVC.swift
+//  GithubProjectBySeanAllen
+//
+//  Created by Dhrushit Raval on 9/10/24.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
